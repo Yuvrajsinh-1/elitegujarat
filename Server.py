0740ddee00b1107c2f00c18f2b64671e525a466a
Yuvrajsinh-1 <220390116027@saffrony.ac.in>
2025-06-27 10:29:35 +0530

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template
-import os
 
-# Tell Flask to look for templates in the current directory
-app = Flask(__name__, template_folder='.')
+app = Flask(__name__)
 
+# Home page
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
+
+# Dynamic route to serve any other HTML file
+@app.route('/<page>')
+def other_pages(page):
+    try:
+        return render_template(f'{page}.html')
+    except:
+        return render_template('404.html'), 404
+
+# Optional: 404 page
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
